77a8fbcdec87c20d488c08b59e86e98861a22c2d
陈光旭 <guangxuchen@oneops.cn>
2019-06-13 15:43:09 +0800

    /getList

--- src/main/java/com/yw/shirotemplate/myShiro/controller/UserController.java
+++ src/main/java/com/yw/shirotemplate/myShiro/controller/UserController.java
@@ -2,13 +2,17 @@ package com.yw.shirotemplate.myShiro.controller;
 
 
 import com.yw.shirotemplate.myShiro.entity.User;
+import com.yw.shirotemplate.myShiro.mapper.UserMapper;
 import com.yw.shirotemplate.myShiro.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -19,15 +23,19 @@ import java.util.List;
  * @since 2019-06-13
  */
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     @Resource
     private IUserService userService;
+    @Autowired
+    UserMapper userMapper;
 
-    @RequestMapping("getList")
+    @RequestMapping("/getList")
     public List<User> getList(){
-        return userService.listOnLineUser();
+        Map map = new HashMap();
+        map.put("start",0);
+        map.put("pageSize",3);
+        return userMapper.getAllUser(map);
     }
 
 }
